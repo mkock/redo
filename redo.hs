f7303fcadc43b093b227b00d86674441625c8ff9
Martin Kock <kock.martin@gmail.com>
2023-03-26 14:56:11 +0200

    Fixed race condition, added command-line option

--- redo.hs
+++ redo.hs
@@ -1,6 +1,12 @@
+import System.Environment
 import System.Process
 
 main = do
-    _ <- createProcess $ shell "fish redo.do"
-    return ()
+  args <- getArgs
+  mapM redo args
 
+redo :: String -> IO ()
+redo target = do
+  (_, _, _, ph) <- createProcess $ shell $ "fish " ++ target ++ ".do"
+  _ <- waitForProcess ph
+  return ()
